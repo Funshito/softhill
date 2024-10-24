b484d8725b63fdcd2e5dacbf5119abcd982e9948
Oluwafunsho Alabi <tuninissi@gmail.com>
2024-10-24 23:35:51 +0100

    mobile nav btn

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('close').addEventListener('click', function () {
+    document.querySelector('.mobile-nav').style.display = 'none';
+    
+})
+
+document.getElementById('menu').addEventListener('click', function(){
+    document.querySelector('.mobile-nav').style.display = 'block';
+})
